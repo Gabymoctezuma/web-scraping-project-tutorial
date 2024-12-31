5673c6e91aab74cde3d306735379d9d8450ee084
Gabymoctezuma <gabiiimp@hotmail.com>
2024-12-31 20:54:20 +0000

    web scraping tesla

--- src/app.py
+++ src/app.py
@@ -6,3 +6,44 @@ import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+html_data = requests.get(url, time.sleep(10)).text
+
+# If no information is extracted, then connect as anonymous
+if "403 Forbidden" in html_data:
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
+    request = requests.get(url, headers = headers)
+    time.sleep(10)
+    html_data = request.text
+
+html_data
+
+
+
+soup = BeautifulSoup(html_data,"html.parser")
+soup
+
+tables = soup.find_all("table")
+tables
+
+import pandas as pd
+
+for index, table in enumerate(tables):
+    if ("Tesla Quarterly Revenue" in str(table)):
+        table_index = index
+        break
+
+# Create a DataFrame
+tesla_revenue = pd.DataFrame(columns = ["Date", "Revenue"])
+for row in tables[table_index].tbody.find_all("tr"):
+    col = row.find_all("td")
+    if (col != []):
+        Date = col[0].text
+        Revenue = col[1].text.replace("$", "").replace(",", "")
+        tesla_revenue = pd.concat([tesla_revenue, pd.DataFrame({
+            "Date": Date,
+            "Revenue": Revenue
+        }, index = [0])], ignore_index = True)
+
+tesla_revenue.head()
